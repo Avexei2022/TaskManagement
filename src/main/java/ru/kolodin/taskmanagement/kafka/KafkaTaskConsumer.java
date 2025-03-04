7dad49cc5db0f96eff8c42137206545eb9c57892
alex123 <alex123@dot.com>
2025-03-04 15:10:52 +0500

    The reviewer's comments have been eliminated

--- src/main/java/ru/kolodin/taskmanagement/kafka/KafkaTaskConsumer.java
+++ src/main/java/ru/kolodin/taskmanagement/kafka/KafkaTaskConsumer.java
@@ -11,6 +11,8 @@ import org.springframework.stereotype.Component;
 import ru.kolodin.taskmanagement.model.task.TaskIdStatusDto;
 import ru.kolodin.taskmanagement.service.notification.NotificationService;
 
+import java.util.List;
+
 @Slf4j
 @RequiredArgsConstructor
 @Component
@@ -21,14 +23,19 @@ public class KafkaTaskConsumer {
     @KafkaListener(id = "${task.kafka.consumer.group-id}",
                     topics = "${task.kafka.topic.task-mng}",
                      containerFactory = "kafkaListenerContainerFactory")
-    public void listener(@Payload TaskIdStatusDto taskIdStatusDto,
+    public void listener(@Payload List<TaskIdStatusDto> taskIdStatusDtos,
                          Acknowledgment ack,
                          @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
                          @Header(KafkaHeaders.RECEIVED_KEY) String key) {
         log.debug("Task consumer: Processing new messages...");
         try {
-            notificationService.toLog(taskIdStatusDto);
-            notificationService.toMail(taskIdStatusDto);
+            taskIdStatusDtos.forEach(
+                    taskIdStatusDto -> {
+                        notificationService.toLog(taskIdStatusDto);
+                        notificationService.toMail(taskIdStatusDto);
+                    }
+            );
+
         } finally {
             ack.acknowledge();
         }
