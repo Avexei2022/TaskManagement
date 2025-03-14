a6c8fb7b99a79efa65d284cfa0f3dbd4c701bc1a
alex123 <alex123@dot.com>
2025-03-14 23:11:36 +0500

    TaskMapperUnitTest and Readme have been edited

--- taskApp/src/test/java/ru/kolodin/taskmanagement/util/TaskMapperUnitTest.java
+++ taskApp/src/test/java/ru/kolodin/taskmanagement/util/TaskMapperUnitTest.java
@@ -1,30 +1,21 @@
 package ru.kolodin.taskmanagement.util;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.kolodin.taskmanagement.model.task.Task;
 import ru.kolodin.taskmanagement.model.task.TaskDto;
 import ru.kolodin.taskmanagement.model.task.TaskStatus;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+
 class TaskMapperUnitTest {
-
-    private Task task;
-    private TaskDto taskDto;
-
-    @BeforeEach
-    void setUp() {
-        task = createTask();
-        taskDto = createTaskDto();
-    }
 
     @Test
     @DisplayName("Тест маппинга ДТО в задачу")
     void toEntity() {
-        Task taskActual = TaskMapper.toEntity(taskDto);
-
-        assertEquals(task, taskActual);
+        Task taskActual = TaskMapper.toEntity(createTaskDto());
+        Task taskExpected = createTask();
+        assertEquals(taskExpected, taskActual);
 
     }
 
@@ -32,25 +23,27 @@ class TaskMapperUnitTest {
     @DisplayName("Тест маппинга задачи в ДТО")
     void toDto() {
 
-        TaskDto taskDtoActual = TaskMapper.toDto(task);
-
-        assertEquals(taskDto, taskDtoActual);
+        TaskDto taskDtoActual = TaskMapper.toDto(createTask());
+        TaskDto taskDtoExpected = createTaskDto();
+        assertEquals(taskDtoExpected, taskDtoActual);
     }
 
     @Test
     @DisplayName("Тест маппинга ДТО в новую задачу")
     void toNewEntity() {
-        Task taskActual = TaskMapper.toNewEntity(taskDto);
-        task.setId(null);
-        assertEquals(task, taskActual);
+        Task taskActual = TaskMapper.toNewEntity(createTaskDto());
+        Task taskExpected = createTask();
+        taskExpected.setId(null);
+        assertEquals(taskExpected, taskActual);
     }
 
     @Test
     @DisplayName("Тест маппинга ДТО в задачу с заменой ID")
     void toUpdateEntityById() {
-        Task taskActual = TaskMapper.toUpdateEntityById(2L, taskDto);
-        task.setId(2L);
-        assertEquals(task, taskActual);
+        Task taskActual = TaskMapper.toUpdateEntityById(2L, createTaskDto());
+        Task taskExpected = createTask();
+        taskExpected.setId(2L);
+        assertEquals(taskExpected, taskActual);
     }
 
     private TaskDto createTaskDto() {
